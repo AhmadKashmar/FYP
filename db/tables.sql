12e444c81e976df06e6a1e19b7bde847c194c8b3
Ahmad Kashmar <AhmadKashmar999@gmail.com>
2025-04-07 18:57:10 +0300

    Added new module
    - added database tables, and a module to migrate csv to db
    - added code to return how long code has been running
    - added example.env as an example of what requirements need to be present

---
+++ db/tables.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS FYP;
+
+USE FYP;
+
+CREATE TABLE
+    IF NOT EXISTS Sentence (
+        sentence_id INT NOT NULL,
+        section_id INT NOT NULL,
+        paragraph_id INT,
+        text TEXT NOT NULL,
+        PRIMARY KEY (sentence_id, section_id)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS Related_text (
+        related_id CHAR PRIMARY KEY,
+        details TEXT NOT NULL,
+        source VARCHAR
+    );
+
+CREATE TABLE
+    IF NOT EXISTS relationship (
+        sentence_id INT NOT NULL,
+        section_id INT NOT NULL,
+        related_text_id CHAR NOT NULL,
+        PRIMARY KEY (sentence_id, section_id, related_text_id),
+        FOREIGN KEY (sentence_id, section_id) REFERENCES Sentence (sentence_id, section_id),
+        FOREIGN KEY (related_text_id) REFERENCES Related_text (related_id)
+    );
